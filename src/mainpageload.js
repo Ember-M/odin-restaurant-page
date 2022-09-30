237e03c388733a8b6c925dc13de163e3aaba4e40
Ember-M <ember.maes@gmail.com>
2022-09-30 04:14:45 -0700

    new file:   dist/09a8ca139230e5bf567e.jpeg 	new file:   dist/725d470122c4d461521d.jpg 	modified:   dist/index.html 	modified:   dist/main.js 	new file:   src/cover.jpeg 	new file:   src/logo.jpg 	modified:   src/mainpageload.js 	modified:   src/menupageload.js     modified:   src/style.css     Added menu items.     Styled menu item format.     Added Main page picture.     Added Main page description.     Set up consistant color theme (may change later).

--- src/mainpageload.js
+++ src/mainpageload.js
@@ -2,13 +2,24 @@ export default function MakeMainPage(){
 
 const mainContainer = document.createElement('div');
 mainContainer.setAttribute('id', 'mainContainer');
-const mainName = document.createElement('div');
-mainName.setAttribute('id', 'mainName');
+
+const mainNameContainer = document.createElement('div');
+mainNameContainer.setAttribute('id', 'mainNameContainer');
+const mainNameText = document.createElement('span');
+mainNameText.innerText = "Welcome to Vida Brillante!";
+mainNameContainer.append(mainNameText)
+
 const mainPictureContainer = document.createElement('div');
 mainPictureContainer.setAttribute('id', 'mainPictureContainer');
+
+
 const mainDescription = document.createElement('div');
 mainDescription.setAttribute('id','mainDescription');
-mainContainer.append(mainName, mainPictureContainer, mainDescription)
+const mainDescriptionText = document.createElement('div');
+mainDescription.innerHTML = "<p>"+ "Thank you for choosing Vida Brilliante, your local source for healthy and ethical food since 2015." + "</p>"+"<p>" + "All of our dishes are made fresh daily, with as many locally sourced ingredients as possible!" + "</p>" + "<p>" + "We pride ourselves on the quality of our food, and our service, and we're so excited for you to visit!"+"</p>"
+// - mainDescription.append(mainDescriptionText)
+
+mainContainer.append(mainNameContainer, mainPictureContainer, mainDescription)
 
 function postContent(){
     document.getElementById('pageContent').appendChild(mainContainer);
--- src/menupageload.js
+++ src/menupageload.js
@@ -1,15 +1,18 @@
 let menuArray = [];
 
-function MakeMenuItem(name,description){
+function MakeMenuItem(name,description,price){
     return{
         name:name,
         description:description,
+        price,
     }
 }
-menuArray.push(MakeMenuItem('Agedashi Tofu',"A plate of delicious fried tofu, garnished with a soy and ginger sauce."));
-menuArray.push(MakeMenuItem('Soba Noodles', "Cold noodles served with a rich and tangy vegan soba sauce."))
-menuArray.push(MakeMenuItem('Ramen',"A warm and filling bowl of soy-miso broth with all the fixin's."))
-menuArray.push(MakeMenuItem('Kimchi',"House-made vegan kimchi from fresh, local-grown Napa cabbage."))
+menuArray.push(MakeMenuItem('Agedashi Tofu',"A plate of delicious fried tofu, garnished with a soy and ginger sauce.", "$5.50"));
+menuArray.push(MakeMenuItem('Soba Noodles', "Cold noodles served with a rich and tangy vegan soba sauce.","$7.50"))
+menuArray.push(MakeMenuItem('Ramen',"A warm and filling bowl of soy-miso broth with all the fixin's.","$10.00"))
+menuArray.push(MakeMenuItem('Kimchi',"House-made vegan kimchi from fresh, local-grown Napa cabbage.","$4.50"))
+menuArray.push(MakeMenuItem("Eggplant Izakaya","Skewers of charcoal-grilled Eggplant, marinated with a tart rice vinegar sauce.", "$5.50"))
+menuArray.push(MakeMenuItem("Bibimbap","A filling rice and seasoned vegetable dish, served in a stone bowl, with a side of banchan.","$11.00"))
 
 
 
@@ -24,14 +27,20 @@ export default function MakeMenuPage(){
     menuNameContainer.append(menuName)
     let menuItems = document.createElement('div');
     menuItems.setAttribute('id', 'menuItems');
+
     menuArray.forEach(element => {
         let menuItemName = document.createElement('div')
         let menuItemDescription = document.createElement('div');
+        let menuItemPrice = document.createElement('div');
         let menuItem = document.createElement('div');
         menuItem.className = 'menu-item';
+        menuItemName.className = "menu-item-name"
+        menuItemDescription.className = "menu-item-description";
+        menuItemPrice.className = "menu-item-price";
         menuItemName.innerText = element.name;
         menuItemDescription.innerText = element.description;
-        menuItem.append(menuItemName,menuItemDescription);
+        menuItemPrice.innerText = element.price;
+        menuItem.append(menuItemName,menuItemDescription,menuItemPrice);
         menuItems.append(menuItem)
     })
 
